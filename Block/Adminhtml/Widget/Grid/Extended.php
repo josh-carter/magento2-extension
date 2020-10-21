2d988d72349b87fc515271943d34d74ff749e443
Jahanzaib Chishti <jahanzaib@strakertranslations.com>
2020-10-21 15:02:55 +1300

    Add more options in the dropdown for max limit selection

---
+++ Block/Adminhtml/Widget/Grid/Extended.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Straker\EasyTranslationPlatform\Block\Adminhtml\Widget\Grid;
+
+class Extended extends \Magento\Backend\Block\Widget\Grid\Extended
+{
+	// Overriding template to insert new functionality
+    protected $_template = 'Straker_EasyTranslationPlatform::widget/grid/extended.phtml';
+}
+
+?>
